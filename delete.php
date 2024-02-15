da655fccae64c4ecb1c6c1e5b718c40628f9aeb5
Gabriel Alves <119307561+Sozim1@users.noreply.github.com>
2024-02-14 23:11:54 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,16 +1,15 @@
 <?php
 include 'database.php';
 
-// Verifica se o ID do registro a ser excluído foi fornecido via GET
+
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Constrói a consulta SQL para excluir o registro
+    
     $sql = "DELETE FROM usuarios WHERE id = $id";
 
-    // Executa a consulta SQL
+    
     if (mysqli_query($conn, $sql)) {
-        // Redireciona para a página inicial após a exclusão
         header("Location: index.php");
         exit();
     } else {
@@ -20,6 +19,5 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
     echo "ID do registro não fornecido.";
 }
 
-// Fecha a conexão com o banco de dados
 mysqli_close($conn);
 ?>
